420db82753ad575bb4c1c12554fe87f2a867de24
Diego Rodriguez <dierocam@gmail.com>
2023-05-30 23:32:14 -0500

    Added a new course for test & Updated the admin to maintain Courses aand Choices

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -23,6 +23,19 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
+admin.site.register(Question)
+admin.site.register(Choice)
+# class QuestionAdmin(admin.ModelAdmin):
+#     list_display = ['question_text']
+#     lession = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+#     # question text
+#     question_text = models.CharField(max_length=200)
+#     # question grade/mark
+#     grade = models.IntegerField(default=0)
+
+# class ChoiseAdmin(admin.ModelAdmin):
+#     list_display = ['title']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
